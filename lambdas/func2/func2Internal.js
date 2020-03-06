c25220667ad13a9c79aa49e76777518cdae0d0aa
Brandon Miller <computerengineer.brandon@gmail.com>
2020-03-06 17:18:01 -0600

    npm debug scripts

--- lambdas/func2/func2Internal.js
+++ lambdas/func2/func2Internal.js
@@ -2,6 +2,8 @@ let math = require(process.env.AWS ? '/opt/nodejs/common' : '../../layers/layer1
 var tuc = require( process.env.AWS ? 'temp-units-conv' :'../../layers/layer1/nodejs/node_modules/temp-units-conv');
 
 exports.tuc = () => {
-    return `temp: ${math.convert(100)}`
-    //return `temp: ${tuc.c2f(math.add(40,60))}`
+    var temp1 = math.convert(100);
+    var temp2 = tuc.c2f(math.add(40,60));
+    var result = `temp1: ${temp1}, temp2: ${temp2}`;
+    return result;
 };
